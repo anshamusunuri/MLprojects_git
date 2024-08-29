801e8c47441cb3c724331fb655568ec284c1d456
aneesham654@gmail.com <aneesham654@gmail.com>
2024-08-29 16:18:40 +0200

    hyperparameter_tunning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -5,7 +5,6 @@ import os
 import sys
 from dataclasses import dataclass
 
-#from catboost import CatBoostRegressor
 from sklearn.ensemble import (
     AdaBoostRegressor,
     GradientBoostingRegressor,
@@ -15,7 +14,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
-#from xgboost import XGBRegressor
+
 
 from src.exception import CustomException
 from src.logger import logging
@@ -29,10 +28,11 @@ class ModelTrainerConfig:
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config=ModelTrainerConfig()
-    
+
+
     def initiate_model_trainer(self,train_array,test_array):
         try:
-            logging.info(f"Spliting the training and test input data")
+            logging.info("Split training and test input data")
             X_train,y_train,X_test,y_test=(
                 train_array[:,:-1],
                 train_array[:,-1],
@@ -44,10 +44,7 @@ class ModelTrainer:
                 "Decision Tree": DecisionTreeRegressor(),
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
-                #"XGBRegressor": XGBRegressor(),
-                #"CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor(),
-
             }
             params={
                 "Decision Tree": {
@@ -70,17 +67,6 @@ class ModelTrainer:
                     'n_estimators': [8,16,32,64,128,256]
                 },
                 "Linear Regression":{},
-                """
-                "XGBRegressor":{
-                    'learning_rate':[.1,.01,.05,.001],
-                    'n_estimators': [8,16,32,64,128,256]
-                },
-                "CatBoosting Regressor":{
-                    'depth': [6,8,10],
-                    'learning_rate': [0.01, 0.05, 0.1],
-                    'iterations': [30, 50, 100]
-                },
-                """
                 "AdaBoost Regressor":{
                     'learning_rate':[.1,.01,0.5,.001],
                     # 'loss':['linear','square','exponential'],
@@ -88,9 +74,11 @@ class ModelTrainer:
                 }
                 
             }
-            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
-           
-           ## To get best model score from dict
+            logging.info("model is tuning")
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
+                                             models=models,params=params)
+            
+            ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
 
             ## To get best model name from dict
@@ -99,6 +87,7 @@ class ModelTrainer:
                 list(model_report.values()).index(best_model_score)
             ]
             best_model = models[best_model_name]
+
             if best_model_score<0.6:
                 raise CustomException("No best model found")
             logging.info(f"Best found model on both training and testing dataset")
@@ -112,9 +101,7 @@ class ModelTrainer:
 
             r2_square = r2_score(y_test, predicted)
             return r2_square
-
-
-
-
+            
         except Exception as e:
             raise CustomException(e,sys)
+        
